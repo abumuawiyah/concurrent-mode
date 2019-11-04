0caf2b40310d213c414924cdededb55244e39a19
Azizi Yazit <ibnuyazit@gmail.com>
2019-11-04 21:51:57 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React, {
+  useDeferredValue,
+  useState,
+  useTransition,
+  Suspense
+} from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import { fetchProfileData } from "./fakeApi";
+
+function getNextId(id) {
+  return id === 3 ? 0 : id + 1;
+}
+
+const initialResource = fetchProfileData(0);
+
+function App() {
+  const [resource, setResource] = useState(initialResource);
+  const [startTransition, isPending] = useTransition({
+    timeoutMs: 3000
+  });
+  return (
+    <>
+      <button
+        disabled={isPending}
+        onClick={() => {
+          startTransition(() => {
+            const nextUserId = getNextId(resource.userId);
+            setResource(fetchProfileData(nextUserId));
+          });
+        }}
+      >
+        Next
+      </button>
+      {isPending ? " Loading..." : null}
+      <ProfilePage resource={resource} />
+    </>
+  );
+}
+
+function ProfilePage({ resource }) {
+  const deferredResource = useDeferredValue(resource, {
+    timeoutMs: 1000
+  });
+  return (
+    <Suspense fallback={<h1>Loading profile...</h1>}>
+      <ProfileDetails resource={resource} />
+      <Suspense fallback={<h1>Loading posts...</h1>}>
+        <ProfileTimeline
+          resource={deferredResource}
+          isStale={deferredResource !== resource}
+        />
+      </Suspense>
+    </Suspense>
+  );
+}
+
+function ProfileDetails({ resource }) {
+  const user = resource.user.read();
+  return <h1>{user.name}</h1>;
+}
+
+function ProfileTimeline({ isStale, resource }) {
+  const posts = resource.posts.read();
+  return (
+    <ul style={{ opacity: isStale ? 0.7 : 1 }}>
+      {posts.map(post => (
+        <li key={post.id}>{post.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(<App />);
